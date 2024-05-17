d410b30a6cb3a147c071883c29989e59a78e0937
vinay18082004 <167321324+vinay18082004@users.noreply.github.com>
2024-05-17 22:32:00 +0530

    Add files via upload

---
+++ ipr46/home.php
@@ -0,0 +1,103 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+    <title>IPR-46(Home)</title>
+</head>
+<body>
+
+    <div class="nav">
+        <div class="logo">
+            <a href="home.php"><img src="imglogo.JPG" alt="not found">
+                <h1>IPR-46 MAPS</a></h1>
+            <ul>
+        </div>
+        <navv>
+            <li class="nav-items"><a href="home.php">Home</a></li>
+            <li class="nav-items"><a href="#services">Services</a></li>
+            <li class="nav-items"><a href="#about">About</a></li>
+            <li class="nav-items"><a href="#contact">Contact Us</a></li>
+            </ul>
+            <div class="right-links">
+              
+                <a href="php/logout.php"><button class="btn">Log Out</button></a>
+                
+                
+
+            </div>
+        </navv>
+        <!-- <img src="imglogo.PNG" alt="not found" class="menu-icon"> -->
+    </div>
+
+
+<div class="section-content">
+
+        <section id="home">
+            <div class="main-content">
+                <h1>This is very Easy</h1>
+                <p>find out your destination using maps</p>
+            </div>
+          
+        </section>
+        
+
+        
+
+
+        <section id="services">
+             <div class="main-content2">
+                <h1>This is services <h1>
+                <p>services Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, eius libero alias, dolorem consequatur harum adipisci maiores dolores, consequuntur dolore quas eligendi nostrum eveniet? Sed voluptas odit repellendus eos vero hic illum, assumenda dicta et unde veniam, porro voluptatum perspiciatis maiores totam quod molestias animi accusamus? Non soluta atque, repudiandae maiores quae, culpa optio dignissimos sequi alias quis saepe ipsam minima sed eligendi nostrum asperiores mollitia. Iusto recusandae magni omnis distinctio saepe et possimus eligendi blanditiis perspiciatis reiciendis suscipit beatae voluptas tempora, debitis assumenda cumque ipsam doloribus impedit eaque neque. Nulla, quis cumque dolore quo tenetur minima quidem recusandae modi?</p>
+
+                <div class="cont">       
+                <button type="button" onclick="window.open('https://www.google.com/maps/@21.0680074,82.7525294,5z?entry=ttu', '_blank')" class="bn" style="--clr:#39FF14">button2</button>
+                <button onclick="window.open('maps/maps/index.html', '_blank')" class="bn" style="--clr:#39FF14">button1</button>
+                <button type="button" onclick="window.open('https://www.youtube.com/', '_blank')" class="bn" style="--clr:#39FF14">button3</button>
+                </div>
+                <h1>sjskjfhj</h1>
+            </div>
+        </section>
+
+
+        <section id="about">
+            <div class="main-content1">
+                <h1>This is about <h1>
+                <p>about</p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolor. Eligendi autem assumenda quidem commodi eum officiis facilis fugit, consequatur nam ratione deleniti labore fuga, velit placeat nulla quis harum officia sit magnam. Dolorem cum dolores eligendi? Sint facere amet necessitatibus perferendis distinctio qui minima aliquam ad quisquam alias animi, recusandae corrupti magni nostrum, voluptatum optio libero fugit. Numquam dignissimos expedita esse voluptatem amet officiis, sed nisi totam, vitae dicta obcaecati deleniti hic? Voluptates in aliquam temporibus ab, alias beatae animi, ddddddddddddd  nostrum enim possimus. In cum non amet optio at, quis sunt deleniti cumque except   uri assumenda, odit sapiente. Temporibus suscipit ex reiciendis. Doloremque necessitatibus nam nisi, reiciendis quidem quisquam in dolorem nulla saepe consequatur dolores quo quas. Perferendis cumque a necessitatibus voluptates illum ipsa eaque, ducimus minus architecto iste sapiente impedit modi deserunt, maiores quas dolore inventore aperiam fuga nihil! Facilis aliquam ea, placeat totam autem magni doloribus iure aut rem laudantium labore sequi asperiores libero sed inventore consequatur ipsa, minima exercitationem quaerat corrupti quo blanditiis magnam delectus! Itaque, magnam numquam! Nulla consequuntur quos accusantium nobis?
+            </div>
+        </section>            
+        
+
+
+        <section id="contact">
+            <div>
+            
+                Lorem ipsum dolor sit <br> amet consectetur adipisicing elit. Exercitationem iure, excepturi, praesentium nulla ad unde provident quas est debitis <br> numquam magnam libero. Sunt corporis inventore nesciunt vero sequi reprehenderit ea vel reiciendis! Culpa sed corrupti <br> quaerat facilis, incidunt, accusamus labore cum eligendi, doloribus tempora ullam quas adipisci animi hic asperiores <br> inventore! Accusamus ad nesciunt maiores veniam voluptate architecto odio nulla esse optio eligendi eum, vel velit <br> dolor? Quia modi itaque dignissimos eius explicabo adipisci rem quisquam delectus, deserunt nemo provident exercitati <br> onem aut maxime? Ipsum aliquid vero, alias eligendi fugit <br> consectetur eaque aspernatur? Quos eveniet minima persp <br> iciatis voluptate nesciunt fuga deserunt!
+            </div>
+            <div class="main-content3">
+                <h1>This is contact <h1>
+                <p>about</p>
+            </div>
+        </section>
+
+    </div>
+
+    <script>
+        var scroll = new SmoothScroll('a[href*="#"]', {
+            speed: 1000,
+            speedAsDuration: true
+        });
+    </script>
+    
+</body>
+
+</html>
